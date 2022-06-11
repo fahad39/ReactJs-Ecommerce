f5db0bf80f37efcd17525a43df600640d5c37b49
fahad <fahad.hussain@thehexaa.com>
2022-06-11 12:17:56 +0500

    added packages and first file

---
+++ src/components/products/Products.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+
+function Products() {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}></Grid>
+    </main>
+  );
+}
+
+export default Products;
