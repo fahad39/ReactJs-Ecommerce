de5eb102ec65b319d3ecbb03fb3e3884112b5f58
fahad <fahad.hussain@thehexaa.com>
2022-06-13 11:34:16 +0500

    added styles and component

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 
+import Product from "./Product/Product";
+
+const products = [
+  { id: 1, name: "Product 1", description: "This is product 1", price: "$10" },
+  { id: 2, name: "Product 2", description: "This is product 2", price: "$120" },
+];
+
 function Products() {
   return (
     <main>
-      <Grid container justify="center" spacing={4}></Grid>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
     </main>
   );
 }
